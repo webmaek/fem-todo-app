82b8570368eda83002507d2389c72bf5fced413e
Kacper Malek <kacper.malek0828@gmail.com>
2021-09-09 17:55:21 +0200

    Add Layout component

---
+++ components/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+const Layout = ({ children }) => {
+  return (
+    <div className='w-full h-screen relative bg-gray-200 dark:bg-fem-dark-blue'>
+      {children}
+    </div>
+  )
+}
+
+export default Layout
