f9d0c26b0af0b9024c27bd40b77cc653f1f4adb5
Kacper Malek <kacper.malek0828@gmail.com>
2021-09-09 17:56:35 +0200

    Add TodoForm component

---
+++ components/TodoForm/TodoForm.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+
+const TodoForm = ({ addTodo }) => {
+  const [text, setText] = useState('')
+
+  const handleSubmit = event => {
+    event.preventDefault()
+
+    if (text !== '' && text.length) {
+      addTodo(text)
+      setText('')
+    }
+  }
+
+  return (
+    <form className='my-6' onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Create a new todo...'
+        className='p-4 w-full rounded-md bg-white dark:bg-fem-blue'
+        value={text}
+        onChange={event => setText(event.target.value)}
+      />
+    </form>
+  )
+}
+
+export default TodoForm
