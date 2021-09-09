a6a50b6ddf971ba9c6b551338701d8229b3132a5
Kacper Malek <kacper.malek0828@gmail.com>
2021-09-09 17:54:54 +0200

    Add Footer component

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <footer className="absolute bottom-0 left-0 w-full p-5 flex items-center justify-center">
+      <p className="text-gray-400">Drag and drop to reorder list</p>
+    </footer>
+  );
+};
+
+export default Footer;
