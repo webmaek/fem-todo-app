8dcf6a6fcf79bca1f87f663ac22a883a17101b2d
webmaek <kacper.malek0828@gmail.com>
2021-09-19 22:17:30 +0200

    Format with prettier

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,21 @@
-const plugin = require('tailwindcss/plugin')
+const plugin = require("tailwindcss/plugin");
 
 module.exports = {
-  mode: 'jit',
-  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class', // or 'media' or 'class'
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['Josefin Sans', 'sans-serif'],
-        serif: ['Josefin Sans', 'serif'],
+        sans: ["Josefin Sans", "sans-serif"],
+        serif: ["Josefin Sans", "serif"],
       },
       colors: {
-        'fem-blue': 'hsl(235, 24%, 19%)',
-        'fem-dark-blue': 'hsl(235, 21%, 11%)',
+        "fem-blue": "hsl(235, 24%, 19%)",
+        "fem-dark-blue": "hsl(235, 21%, 11%)",
       },
-      backgroundImage: theme => ({
-        'desktop-light': `url('/images/bg-desktop-light.jpg')`,
+      backgroundImage: (theme) => ({
+        "desktop-light": `url('/images/bg-desktop-light.jpg')`,
       }),
     },
   },
@@ -23,4 +23,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
