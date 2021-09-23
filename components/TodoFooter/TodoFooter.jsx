466a8f602598c3ca5d1d3d69ff93a299965b7ab4
webmaek <kacper.malek0828@gmail.com>
2021-09-23 15:41:47 +0200

    Fix imports, change display styles, import useTodoContext

--- components/TodoFooter/TodoFooter.jsx
+++ components/TodoFooter/TodoFooter.jsx
@@ -1,29 +1,33 @@
-const TodoFooter = ({
-  setShowActive,
-  setShowCompleted,
-  clearCompleted,
-  todosLength,
-}) => {
+import { useTodoContext } from "../../contexts/TodoContext";
+
+const TodoFooter = () => {
+  const { todos, toggleActive, toggleCompleted, clearCompleted } =
+    useTodoContext();
+
   const handleAllClick = () => {
-    setShowActive(false);
-    setShowCompleted(false);
+    toggleActive(false);
+    toggleCompleted(false);
   };
 
   const handleActiveClick = () => {
-    setShowActive(true);
-    setShowCompleted(false);
+    toggleActive(true);
+    toggleCompleted(false);
   };
 
   const handleCompletedClick = () => {
-    setShowActive(false);
-    setShowCompleted(true);
+    toggleActive(false);
+    toggleCompleted(true);
   };
 
   return (
-    <div className="dark:bg-fem-blue dark:text-white pt-2">
+    <div
+      className={`dark:bg-fem-blue bg-white dark:text-white pt-2 rounded-b-md shadow-xl ${
+        todos.length ? "block" : "hidden"
+      }`}
+    >
       <div className="flex items-center justify-between p-2">
         <div>
-          <span className="text-sm font-light">{todosLength} items left</span>
+          <span className="text-sm font-light">{todos.length} items left</span>
         </div>
         <div>
           <button className="text-blue-500" onClick={handleAllClick}>
