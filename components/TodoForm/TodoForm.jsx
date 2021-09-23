582918f643c92268dc2d7fd5af8b33f493f682b3
webmaek <kacper.malek0828@gmail.com>
2021-09-23 15:42:22 +0200

    Add box-shadow to input,  use TodoContext

--- components/TodoForm/TodoForm.jsx
+++ components/TodoForm/TodoForm.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
+import { useTodoContext } from "../../contexts/TodoContext";
 
-const TodoForm = ({ addTodo }) => {
+const TodoForm = () => {
   const [text, setText] = useState("");
+  const { addTodo } = useTodoContext();
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -17,7 +19,7 @@ const TodoForm = ({ addTodo }) => {
       <input
         type="text"
         placeholder="Create a new todo..."
-        className="p-4 w-full rounded-md bg-white dark:bg-fem-blue dark:text-white"
+        className="p-4 w-full rounded-md bg-white dark:bg-fem-blue dark:text-white shadow-xl"
         value={text}
         onChange={(event) => setText(event.target.value)}
       />
