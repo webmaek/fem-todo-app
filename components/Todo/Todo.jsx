7474cca644698527bfb4cb299e570ed3a53707bf
webmaek <kacper.malek0828@gmail.com>
2021-09-23 15:41:10 +0200

    Clean up and code refactor

--- components/Todo/Todo.jsx
+++ components/Todo/Todo.jsx
@@ -1,30 +1,16 @@
-import { useState } from "react";
-import { useTodos } from "../../hooks/useTodos";
-
 import TodoForm from "../TodoForm/TodoForm";
 import TodoList from "../TodoList/TodoList";
+import TodoFooter from "../TodoFooter/TodoFooter";
 import Header from "./Header";
 
 const Todo = () => {
-  const [showActive, setShowActive] = useState(false);
-  const [showCompleted, setShowCompleted] = useState(false);
-  const { todos, addTodo, removeTodo, toggleTodo, clearCompleted } = useTodos();
-
   return (
-    <main className="absolute top-0 left-1/2 -translate-x-1/2 w-2/5">
-      <div className="p-4 py-24">
+    <main className="absolute top-0 w-full h-full">
+      <div className="p-4 py-24 w-2/5 mx-auto">
         <Header />
-        <TodoForm addTodo={addTodo} />
-        <TodoList
-          todos={todos}
-          removeTodo={removeTodo}
-          toggleTodo={toggleTodo}
-          showActive={showActive}
-          showCompleted={showCompleted}
-          setShowActive={setShowActive}
-          setShowCompleted={setShowCompleted}
-          clearCompleted={clearCompleted}
-        />
+        <TodoForm />
+        <TodoList />
+        <TodoFooter />
       </div>
     </main>
   );
