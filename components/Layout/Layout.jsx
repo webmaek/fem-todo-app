267437706ad1e45ee260b24b769942f1059a4185
webmaek <kacper.malek0828@gmail.com>
2021-09-23 15:40:57 +0200

    Use TodoProvider to wrap children

--- components/Layout/Layout.jsx
+++ components/Layout/Layout.jsx
@@ -1,7 +1,9 @@
+import { TodoProvider } from "../../contexts/TodoContext";
+
 const Layout = ({ children }) => {
   return (
-    <div className="w-full h-screen relative bg-gray-300 dark:bg-fem-dark-blue">
-      {children}
+    <div className="w-full h-screen relative bg-gray-200 dark:bg-fem-dark-blue">
+      <TodoProvider>{children}</TodoProvider>
     </div>
   );
 };
